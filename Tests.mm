a5faf15ab7a49a223f821f484652e70ecb4944b5
Justinian Oprescu <ojusti@yahoo.fr>
2015-08-26 17:42:03 +0300

    Initial plan

---
+++ Tests.mm
@@ -0,0 +1,152 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1440489508710" ID="ID_70276483" MODIFIED="1440512118676" TEXT="Tests">
+<node CREATED="1440489518900" ID="ID_47584054" MODIFIED="1440513899433" POSITION="right" TEXT="Property-based testing">
+<icon BUILTIN="full-2"/>
+<node CREATED="1440489551502" ID="ID_1253115672" MODIFIED="1440489559386" TEXT="Number of possibilities">
+<node CREATED="1440489935769" ID="ID_721053762" LINK="http://www.slideshare.net/alexboly/unit-testingpatterns" MODIFIED="1440490109293" TEXT="Exemple: sameNumber, sum, date"/>
+<node CREATED="1440490330485" ID="ID_318269265" MODIFIED="1440490336706" TEXT="Implicit dependencies"/>
+</node>
+<node CREATED="1440489561156" ID="ID_515113874" MODIFIED="1440491300238" TEXT="White-box">
+<node CREATED="1440489578549" ID="ID_770801742" MODIFIED="1440489584274" TEXT="Classes of equivalence"/>
+<node CREATED="1440491300224" ID="ID_1583712241" MODIFIED="1440491322844" TEXT="Dependencies again">
+<node CREATED="1440491328294" ID="ID_816154773" MODIFIED="1440491388523" TEXT="Test case &lt;-&gt; system under test &lt;-&gt; dependencies"/>
+<node CREATED="1440490360040" ID="ID_1752968314" MODIFIED="1440491483066" TEXT="Mocking. Exemple Mockito"/>
+</node>
+</node>
+</node>
+<node CREATED="1440490037433" ID="ID_1555336938" LINK="http://www.slideshare.net/alexboly/unit-testingpatterns" MODIFIED="1440515807488" POSITION="left" TEXT="Risk based testing">
+<icon BUILTIN="full-4"/>
+<node CREATED="1440514040305" ID="ID_319992908" LINK="http://www.thoughtworks.com/insights/blog/are-test-coverage-metrics-overrated" MODIFIED="1440516045649" TEXT="Code coverage"/>
+<node CREATED="1440490400343" ID="ID_539887367" LINK="http://www.hoylen.com/photos/2009-west/09-turin-parma/pic15.html" MODIFIED="1440507844814">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Concrete walls
+    </p>
+    <p>
+      http://netdost.com/profiles/blogs/concrete-meets-car-long-term-parking-by-french-artist-arman
+    </p>
+    <p>
+      http://www.worldtvpc.com/blog/wp-content/uploads/2012/09/regents_open_air_theatre.jpg
+    </p>
+  </body>
+</html></richcontent>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_539887367" ENDARROW="Default" ENDINCLINATION="291;0;" ID="Arrow_ID_1035774702" SOURCE="ID_1684656728" STARTARROW="None" STARTINCLINATION="291;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_539887367" ENDARROW="Default" ENDINCLINATION="1432;0;" ID="Arrow_ID_1488395330" SOURCE="ID_309342877" STARTARROW="None" STARTINCLINATION="1432;0;"/>
+</node>
+</node>
+<node CREATED="1440490218954" ID="ID_1522846812" LINK="http://www.slideshare.net/alexboly/unit-testingpatterns" MODIFIED="1440516067454" POSITION="right" TEXT="Bring legacy code under test">
+<icon BUILTIN="full-5"/>
+<node CREATED="1440507517574" ID="ID_1339822201" MODIFIED="1440507524659" TEXT="Is it testable?"/>
+<node CREATED="1440512451852" ID="ID_648290948" MODIFIED="1440512465303" TEXT="Write tests before fixing bugs">
+<node CREATED="1440512476971" ID="ID_865862220" MODIFIED="1440512493591" TEXT="Confirmation"/>
+</node>
+<node CREATED="1440515041185" ID="ID_51995756" MODIFIED="1440515375991" TEXT="Small safe! refactorings">
+<node CREATED="1440515068841" ID="ID_1178269352" MODIFIED="1440515086541" TEXT="Rename"/>
+<node CREATED="1440515087152" ID="ID_566916324" MODIFIED="1440515106764" TEXT="Extract class from huge method"/>
+<node CREATED="1440515107520" ID="ID_417102958" MODIFIED="1440515125228" TEXT="Extract method"/>
+</node>
+<node CREATED="1440515377127" ID="ID_1003322750" MODIFIED="1440515387122" TEXT="Extract dependencies"/>
+<node CREATED="1440515448822" ID="ID_1379533941" MODIFIED="1440515451058" TEXT="MichaelFeathers&apos; book WorkingEffectivelyWithLegacyCode."/>
+</node>
+<node CREATED="1440490437117" ID="ID_233563964" LINK="http://www.slideshare.net/frankappel98/clean-unit-test-patterns-40762771" MODIFIED="1440514542642" POSITION="right" TEXT="Readability. Find Copilote exemple">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_233563964" ENDARROW="Default" ENDINCLINATION="297;0;" ID="Arrow_ID_1523046823" SOURCE="ID_1703821797" STARTARROW="None" STARTINCLINATION="297;0;"/>
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1440490496733" ID="ID_390849595" MODIFIED="1440490510634" TEXT="Structure. given-when-then"/>
+<node CREATED="1440490528388" ID="ID_793799655" MODIFIED="1440490631950" TEXT="Complex tests">
+<node CREATED="1440490641141" ID="ID_838638289" MODIFIED="1440491012319" TEXT="One behavior per test"/>
+<node CREATED="1440507943180" ID="ID_1252139936" MODIFIED="1440507970928" TEXT="No if, no loops"/>
+<node CREATED="1440490544117" ID="ID_97344450" MODIFIED="1440490604097" TEXT="Builders, Object mother, "/>
+<node CREATED="1440491051697" ID="ID_1774288324" MODIFIED="1440491063069" TEXT="Refactor"/>
+<node CREATED="1440491182858" ID="ID_1123472287" MODIFIED="1440491190756" TEXT="Expected exceptions"/>
+<node CREATED="1440491215999" ID="ID_1546180747" MODIFIED="1440491238639" TEXT="Rules">
+<arrowlink COLOR="#b0b0b0" DESTINATION="ID_576206990" ENDARROW="Default" ENDINCLINATION="595;0;" ID="Arrow_ID_907013577" STARTARROW="None" STARTINCLINATION="595;0;"/>
+</node>
+<node CREATED="1440513916889" ID="ID_118215305" MODIFIED="1440514099980" TEXT="Dependencies">
+<node CREATED="1440491104513" ID="ID_761276700" MODIFIED="1440491112916" TEXT="Setup patterns"/>
+</node>
+</node>
+<node CREATED="1440491439430" ID="ID_1867954173" MODIFIED="1440491456131" TEXT="Assertions">
+<node CREATED="1440491458678" ID="ID_1511521215" MODIFIED="1440491475659" TEXT="JUnit"/>
+<node CREATED="1440491462742" ID="ID_703880285" MODIFIED="1440491465708" TEXT="Hamcrest"/>
+<node CREATED="1440491466510" ID="ID_800655636" MODIFIED="1440491470506" TEXT="AssertJ"/>
+</node>
+<node CREATED="1440507647292" ID="ID_1477192176" MODIFIED="1440507653841" TEXT="Naming conventions">
+<node CREATED="1440507659837" ID="ID_877471281" MODIFIED="1440507727361" TEXT="class name: &lt;Feature&gt;Test"/>
+<node CREATED="1440507727947" ID="ID_309342877" MODIFIED="1440507844813">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      method name: aVeryLongAndDescriptiveNameThatMayNotStartWithTest
+    </p>
+    <p>
+      or a_very_long_and_descriptive_name_that_may_not_start_with_test
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink COLOR="#b0b0b0" DESTINATION="ID_539887367" ENDARROW="Default" ENDINCLINATION="1432;0;" ID="Arrow_ID_1488395330" STARTARROW="None" STARTINCLINATION="1432;0;"/>
+</node>
+<node CREATED="1440511983871" ID="ID_1552445512" MODIFIED="1440512001777" TEXT="@Before setUp / @After tearDown"/>
+</node>
+</node>
+<node CREATED="1440512118654" ID="ID_1408666342" MODIFIED="1440516078886" POSITION="left" TEXT="Integration tests">
+<icon BUILTIN="full-6"/>
+<node CREATED="1440512216950" ID="ID_1017765147" MODIFIED="1440512228113" TEXT="Code + Database">
+<node CREATED="1440491225359" ID="ID_576206990" MODIFIED="1440514530866" TEXT="TestDBResource">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_576206990" ENDARROW="Default" ENDINCLINATION="595;0;" ID="Arrow_ID_907013577" SOURCE="ID_1546180747" STARTARROW="None" STARTINCLINATION="595;0;"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node CREATED="1440512229979" ID="ID_981747049" MODIFIED="1440512238385" TEXT="Code + Other code"/>
+<node CREATED="1440512253068" ID="ID_1235056538" MODIFIED="1440512307680" TEXT="Mock away your dependencies =&gt; Unit tests"/>
+</node>
+<node CREATED="1440501317954" ID="ID_488187891" LINK="http://www.masukomi.org/talks/unit_testing" MODIFIED="1440513848906" POSITION="right" TEXT="Excuses">
+<icon BUILTIN="full-1"/>
+<node CREATED="1440501342586" ID="ID_1376051175" MODIFIED="1440501351317" TEXT="I don&apos;t have time to write tests"/>
+<node CREATED="1440501364553" ID="ID_564915215" MODIFIED="1440501375582" TEXT="I could be coding new features instead">
+<node CREATED="1440502177364" ID="ID_1822798866" LINK="http://www.masukomi.org/talks/unit_testing/#25" MODIFIED="1440502200915" TEXT="Bad news: Average project time"/>
+<node CREATED="1440502220171" ID="ID_1828689201" MODIFIED="1440502228184" TEXT="Minimise maintenance"/>
+</node>
+<node CREATED="1440506078847" ID="ID_483616476" MODIFIED="1440506088373" TEXT="No-one else is writing them"/>
+<node CREATED="1440506123136" ID="ID_174299665" MODIFIED="1440506132045" TEXT="Manual testing is good enough">
+<node CREATED="1440506152575" ID="ID_1529015242" MODIFIED="1440506181337" TEXT="How much time do you have?"/>
+<node CREATED="1440506200423" ID="ID_1270698266" MODIFIED="1440506227059" TEXT="Do you test everything everytime?"/>
+</node>
+</node>
+<node CREATED="1440506338583" ID="ID_1831798699" MODIFIED="1440516154407" POSITION="left" TEXT="Reasons to write tests">
+<icon BUILTIN="clock"/>
+<node CREATED="1440506360054" ID="ID_1731100002" MODIFIED="1440506366898" TEXT="Reduce bugs in new features"/>
+<node CREATED="1440506367967" ID="ID_503397735" MODIFIED="1440506380850" TEXT="Catch stupid mistakes earlier"/>
+<node CREATED="1440506413190" ID="ID_265081347" MODIFIED="1440506421658" TEXT="Reduce bugs in existing features"/>
+<node CREATED="1440506435485" ID="ID_1970207760" MODIFIED="1440507103854" TEXT="Provide (executable) documentation &amp; examples"/>
+<node CREATED="1440507121257" ID="ID_1684656728" MODIFIED="1440507134832" TEXT="Reduce cost of change">
+<arrowlink COLOR="#b0b0b0" DESTINATION="ID_539887367" ENDARROW="Default" ENDINCLINATION="291;0;" ID="Arrow_ID_1035774702" STARTARROW="None" STARTINCLINATION="291;0;"/>
+</node>
+<node CREATED="1440507213192" ID="ID_1668588822" MODIFIED="1440507311118" TEXT="Improve design">
+<node CREATED="1440507229448" ID="ID_1375278677" MODIFIED="1440507239684" TEXT="Hard to test == Too complex"/>
+</node>
+<node CREATED="1440507312672" ID="ID_1693323425" MODIFIED="1440507389325" TEXT="Force you to slow down and think">
+<node CREATED="1440507349416" ID="ID_1666461003" MODIFIED="1440507380531" TEXT="Test each feature as it&apos;s written when it is fresh"/>
+</node>
+<node CREATED="1440507390279" ID="ID_339031910" MODIFIED="1440507393435" TEXT="Reduce fear"/>
+</node>
+<node CREATED="1440511940263" ID="ID_1066716242" MODIFIED="1440516170838" POSITION="right" TEXT="Maintenance of tests">
+<icon BUILTIN="full-7"/>
+<node CREATED="1440511948671" ID="ID_1703821797" MODIFIED="1440511959678" TEXT="Readability">
+<arrowlink DESTINATION="ID_233563964" ENDARROW="Default" ENDINCLINATION="297;0;" ID="Arrow_ID_1523046823" STARTARROW="None" STARTINCLINATION="297;0;"/>
+</node>
+<node CREATED="1440511971110" ID="ID_1536334537" MODIFIED="1440512087171" TEXT="Interdependence vs Isolation">
+<node CREATED="1440512032446" ID="ID_753164028" MODIFIED="1440512578151" TEXT="Beware of static"/>
+</node>
+</node>
+</node>
+</map>
